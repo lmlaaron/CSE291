e61c19d2b46fd7ce7390e8ef6cab70d6ac88e283
Chenwei <caichenwei1991@gmail.com>
2016-06-01 17:35:19 -0700

    modify the output pattern

--- project/project3/batch.sh
+++ project/project3/batch.sh
@@ -12,12 +12,9 @@ $HADOOP_PREFIX/bin/hdfs dfs -rm -r bigram;
 rm -rf bigram;
 $HADOOP_PREFIX/bin/hadoop jar bigramcount.jar $1 bigram;
 $HADOOP_PREFIX/bin/hdfs dfs -get bigram bigram
-line=$(wc -l < bigram/part-*);
-echo "====================================="
-echo "Total number of distinct bigrams = " $line
-echo "====================================="
+line1=$(wc -l < bigram/part-*);
 make clean;
-rm -rf bigram;
+#rm -rf bigram;
 cd ..;
 
 cd TopNBigramCount;
@@ -26,16 +23,8 @@ $HADOOP_PREFIX/bin/hdfs dfs -rm -r topn;
 rm -rf topn;
 $HADOOP_PREFIX/bin/hadoop jar topnbigramcount.jar $1 topn;
 $HADOOP_PREFIX/bin/hdfs dfs -get topn topn
-echo "====================================="
-echo "The most popular bigram = " $(head -n 1 topn/part-* | awk '{print $1 " " $2;}')
-echo "====================================="
-
-echo "====================================="
-echo "Top 10 bigrams and their population"
-cat topn/part-*;
-echo "====================================="
 make clean;
-rm -rf topn;
+#rm -rf topn;
 cd ..;
 
 cd TenPercentBigram;
@@ -44,10 +33,34 @@ $HADOOP_PREFIX/bin/hdfs dfs -rm -r ten;
 rm -rf ten;
 $HADOOP_PREFIX/bin/hadoop jar tenpercenbigram.jar $1 ten;
 $HADOOP_PREFIX/bin/hdfs dfs -get ten ten
-line=$(wc -l < ten/part-*);
-echo "======================"
-echo "Top 10% bigrams = " $line
-echo "======================"
+line3=$(wc -l < ten/part-*);
 make clean;
+#rm -rf ten;
+cd ..;
+
+echo "====================================="
+echo "Total number of distinct bigrams = " $line1
+echo "====================================="
+printf "\n"
+echo "====================================="
+echo "The most popular bigram = " $(head -n 1 TopNBigramCount/topn/part-* | awk '{print $1 " " $2;}')
+echo "====================================="
+printf "\n"
+echo "====================================="
+echo "Top 10 bigrams and their population"
+cat TopNBigramCount/topn/part-*;
+echo "====================================="
+printf "\n"
+echo "======================"
+echo "Top 10% bigrams = " $line3
+echo "======================"
+printf "\n"
+cd BigramCount;
+rm -rf bigram;
+cd ..;
+cd TopNBigramCount;
+rm -rf topn;
+cd ..;
+cd TenPercentBigram;
 rm -rf ten;
 cd ..;
